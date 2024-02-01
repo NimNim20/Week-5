c6f4f698fca282d5e2fbd64e3b053380240454be
NimNim20 <benvio01@easv365.dk>
2024-02-01 11:56:29 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// function changeColor(event){
+//   document.documentElement.style.setProperty('--color-primary', event.target.value);
+// }
+
+const changeColor = (event) => {
+  document.documentElement.style.setProperty('--color-primary', event.target.value);
+}
+
+
+
+
+
+
