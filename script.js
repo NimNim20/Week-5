52c097b1afad450535290456435b258744383558
NimNim20 <benvio01@easv365.dk>
2024-02-02 21:43:48 +0100

    Assignment 3

--- script.js
+++ script.js
@@ -2,9 +2,15 @@
 //   document.documentElement.style.setProperty('--color-primary', event.target.value);
 // }
 
-const changeColor = (event) => {
-  document.documentElement.style.setProperty('--color-primary', event.target.value);
+document.body.onclick = (e) => {
+  const body = document.body;
+
+  body.classList.toggle('dark-mode');
 }
+
+// const changeColor = (event) => {
+//   document.documentElement.style.setProperty('--color-dark-mode', event.target.value);
+// }
 
 
 
