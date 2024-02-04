81f63bb24eb18732375a57a01cbfdfe43b410118
Benjamin Vid Gard <benvio01@easv365.dk>
2024-02-04 16:46:39 +0100

    Fixed button

--- script.js
+++ script.js
@@ -1,71 +1,13 @@
-document.body.onclick = (e) => {
+const changeColor = (event) =>{
+  document.documentElement.style.setProperty('--color-primary', event.target.value);
+}
+
+const darkMode = () => {
   const body = document.body;
-
-  // Toggle dark mode class when the body is clicked
   body.classList.toggle('dark');
-};
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function changeColor(event){
-//   document.documentElement.style.setProperty('--color-primary', event.target.value);
-// }
-
-// const body = document.body;
-// const themeToggle = document.getElementById('dark');
-// const themeToggle = document.getElementById('light');
-
-// themeToggle.onclick = () => {
-//   body.classList.replace('light', 'dark');
-// }
-
-// themeToggle.onclick = () => {
-//   body.classList.replace('dark', 'light');
-// }
-
-// document.body.onclick = (e) => {
-//   const body = document.body;
-
-//   // Toggle dark mode class when the body is clicked
-//   body.classList.replace('light', 'dark');
-// };
-
-
-// const changeColor = (event) => {
-//   document.documentElement.style.setProperty('--color-dark-mode', event.target.value);
-// }
-
-// const changeColor = (event) => {
-//   document.documentElement.style.setProperty('--color-dark-mode', event.target.value);
-// }
-
+document.getElementById('themeToggle').onclick = darkMode;
 
 
 
